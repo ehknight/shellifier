7dd014fe36ecd10ac3fd2f342d6175db3a159f77
hyperdo <super.ethan@gmail.com>
2016-04-06 21:32:30 -0700

    changed port

--- main.py
+++ main.py
@@ -19,5 +19,6 @@ def upload():
     return render_template('index.html', shellified=shelly(preshellified, not advanced))
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port = 33507)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port = port)
 
