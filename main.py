5be08099370514dea62c2f930a11f63d7aef6da6
hyperdo <super.ethan@gmail.com>
2016-04-06 20:00:07 -0700

    fixed not syntax

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def upload():
         advanced=True
     else:
         advanced=False
-    return render_template('index.html', shellified=shelly(preshellified, !advanced))
+    return render_template('index.html', shellified=shelly(preshellified, not advanced))
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port = 80)
+
